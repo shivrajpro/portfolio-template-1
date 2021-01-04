0f9264277a60cc9c5204650b0a3d7538d16d1e83
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-01-04 19:43:25 +0530

    done with tutorial

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const hamburger =  document.querySelector('.header .hamburger');
 const mobile_menu = document.querySelector('.header .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-list ul li a');
+const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+})
+
+document.addEventListener('scroll', ()=>{
+    let scroll_position = window.scrollY;
+    
+    if(scroll_position > 250)
+    header.style.backgroundColor = '#29323c';
+    else
+    header.style.backgroundColor = 'transparent';
+})
+
+menu_item.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
 })
